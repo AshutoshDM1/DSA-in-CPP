faa0f5fa668d538b5a618996296c3cda715f85a2
AshutoshDM1 <downlodemaster1@gmail.com>
2024-09-04 22:47:00 +0530

    Completed Day22

--- Day22/1code.cpp
+++ Day22/1code.cpp
@@ -1,5 +1,11 @@
 #include<iostream>
 using namespace std;
+
+// Time Complexity: part-1
+// this day was theoritcal day for me to understand the theory behind it.
+// There was not any code for this  Day 
+
+
 int main() {
 	cout << "Hello World!" << endl;
 }
