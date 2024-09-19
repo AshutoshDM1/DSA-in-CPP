bfb5af0e98acf53973810754b467671ec10aa1c6
AshutoshDM1 <downlodemaster1@gmail.com>
2024-09-19 22:32:26 +0530

    Day24 20%

--- Day24/1code.cpp
+++ Day24/1code.cpp
@@ -1,6 +1,35 @@
 #include <iostream>
+#include<string>
+#include<vector>
 using namespace std;
+
+void printArr (int arr[] , int n ) {
+    cout << "[";
+    for ( int i = 0 ; i < n ; ++i ) {
+        cout << arr[i] << ",";
+    }
+    cout << "]\n";
+}
+
+void changeArr ( int *arr , int n , int i ) {
+    if (i >= n) {
+        cout << "Recursive Array is : ";
+        printArr (arr , n );
+        return ;
+    }
+    arr[i] = i+1;
+    changeArr ( arr , n , i+1);
+    arr[i] -= 2;
+}
+
 int main() {
-    cout << "Enter the number " << endl;
+    cout << "BackTracking on Array " << endl;
+    int arr[5] = {0};
+    int n = 5;
+    cout << "Initial Array is : ";
+    printArr(arr ,n);
+    changeArr(arr, n  ,0);
+    cout << "BackTracked Array is : ";
+    printArr(arr ,n);
     return 0;
 }
